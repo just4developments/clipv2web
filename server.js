1fd3eaf54dd2b65dd08990f1389e8ec493d76551
thanhdt@viosoft <thanhdt@viosoft.com>
2016-07-15 17:46:20 +0700

    [UPDATE] cached and gzip index.html

--- server.js
+++ server.js
@@ -1,47 +1,40 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let fs = require('fs');
+let app = express();
+var etag = require('etag');
+var compression = require('compression');
 
-
+app.use(compression({ threshold: 0 }));
 app.use(express.static(__dirname + '/dist'));
 
+let page = (p, res) => {
+ res.sendFile(p, { root: __dirname + '/dist'});
+};
+
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/dist/index.html');
+  page('index.html', res);
 });
 
 app.get('/v/:mode', function(req, res){
-  res.sendFile(__dirname + '/dist/index.html');
+  page('index.html', res);
 });
 
 app.get('/k/:keyword', function(req, res){
-  res.sendFile(__dirname + '/dist/index.html');
+  page('index.html', res);
 });
 
 app.get('/search/:txtSearch', function(req, res){
-  res.sendFile(__dirname + '/dist/index.html');
+  page('index.html', res);
 });
 
 app.get('/my-video', function(req, res){
-  res.sendFile(__dirname + '/dist/index.html');
+  page('index.html', res);
 });
 
 app.get('/:id', function(req, res){
-	// res.set('Content-Type', 'text/html');
-	// res.send(`
-	// 	<!DOCTYPE html>
-	// 	<html ng-app='myApp'>
-	// 		<head prefix="og: http://ogp.me/ns#">
-	// 			<meta property="og:type" content="article" />
-	// 			<meta prefix="fb: http://ogp.me/ns/fb#" property="fb:app_id" content="1506977592847257" />
-	// 			<meta property="og:url" content="http://kenh14.vn/news-2016070811445743.chn" />
-	// 			<meta property="og:title" content="Hot: Cảnh sát Gangnam tuyên bố ADN trên tang vật đầu tiên trùng khớp với Yoochun (JYJ)" />
-	// 			<meta property="og:description" content="Ngoài ra, Dispatch vừa đưa tin rằng, có vẻ cảnh sát sẽ sớm đưa ra kết luận vô tội đối với Yoochun ở vụ kiện thứ nhất, bởi rất khó tìm được chứng cứ của hành vi xâm hại hay cưỡng chế dù đúng thật là có quan hệ." />
-	// 			<meta property="og:image" content="http://kenh14.mediacdn.vn/thumb_w/600/2016/20160630184611-ijn-4391-1467282550642-1467953065437-103-0-413-600-crop-1467953079818.jpg" />
-	// 	  </head>
-	//  	</html>		
-	// `);
-  res.sendFile(__dirname + '/dist/index.html');
+	page('index.html', res);
 });
 
 app.listen(80, function () {
-  console.log('Example app listening on port 80!');
+  console.log('clipv2web listening on port 80!');
 });
